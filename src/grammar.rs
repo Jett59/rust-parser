2414f39be61642bfae13e2285ac6ef37768e588c
Jett Thompson <jett.thompson.59@gmail.com>
2023-12-08 17:27:50 +1100

    fix a couple of bugs in the macro syntax definition and add the ability to print a grammar using Debug

--- src/grammar.rs
+++ src/grammar.rs
@@ -1,3 +1,5 @@
+use std::fmt::{self, Debug, Formatter};
+
 pub trait ParseResult<SymbolKind> {
     fn kind(&self) -> SymbolKind;
 }
@@ -7,34 +9,48 @@ pub struct GrammarRule<ParseResult: self::ParseResult<SymbolKind>, SymbolKind> {
     pub reduce: fn(Vec<ParseResult>) -> ParseResult,
 }
 
+impl<ParseResult: self::ParseResult<SymbolKind>, SymbolKind: Debug> Debug
+    for GrammarRule<ParseResult, SymbolKind>
+{
+    fn fmt(&self, fmt: &mut Formatter<'_>) -> fmt::Result {
+        fmt.debug_struct("GrammarRule")
+            .field("input", &self.input)
+            .finish()
+    }
+}
+
+#[macro_export]
 macro_rules! append_grammar_rules {
     ($rules_list:ident, $SymbolKind:ty, $ParseResult:ty, $result_symbol_kind:ident, $($symbol_kind:ident$(:$name:ident)?)* => $reduce:expr) => {{
         fn reduce(symbols: Vec<$ParseResult>) -> $ParseResult {
             let mut symbol_iterator = symbols.into_iter();
             $($(let $name =)? symbol_iterator.next().unwrap();)*
             let result = $reduce;
-            assert_eq!(result.kind(), <$SymbolKind>::$result_symbol_kind, "Grammar rule returned wrong kind");
+            assert_eq!($crate::grammar::ParseResult::kind(&result), <$SymbolKind>::$result_symbol_kind, "Grammar rule returned wrong kind");
             result
         }
-        $rules_list.push(GrammarRule::<$ParseResult, $SymbolKind> {
+        $rules_list.push($crate::grammar::GrammarRule::<$ParseResult, $SymbolKind> {
             input: vec![$(<$SymbolKind>::$symbol_kind),*],
             reduce,
         });
     }};
     ($rules_list:ident, $SymbolKind:ty, $ParseResult:ty, $result_symbol_kind:ident, { $($($symbol_kind:ident$(:$name:ident)?)* => $reduce:expr),* }) => {
         $(
-            append_grammar_rules!($rules_list, $SymbolKind, $ParseResult, $result_symbol_kind, $($symbol_kind$(:$name)?)* => $reduce);
+            $crate::grammar::append_grammar_rules!($rules_list, $SymbolKind, $ParseResult, $result_symbol_kind, $($symbol_kind$(:$name)?)* => $reduce);
         )*
     };
 }
 
+pub use append_grammar_rules;
+
+#[macro_export]
 macro_rules! grammar {
     ($visibility:vis grammar $Grammar:ident {
         symbol_kind: $SymbolKind:ident,
         tokens: [$($token:ident),*],
         parse_result: $ParseResult:ident,
         rules: {$(
-            $NonTerminal:ident: $first:tt $($($optional_rule_tokens:ident)* => $optional_rule_reduce:expr)?
+            $NonTerminal:ident: $({$($grouped_rule_tokens:tt)*})? $($($optional_rule_symbol_kinds:ident$(:$optional_rule_symbol_names:ident)?)* => $optional_rule_reduce:expr)?
     ),*$(,)?}
     }) => {
         #[derive(Clone, Copy, Debug, PartialEq)]
@@ -43,21 +59,24 @@ macro_rules! grammar {
             $($NonTerminal,)*
         }
 
+        #[derive(Debug)]
         $visibility struct $Grammar {
-            pub rules: Vec<GrammarRule<$ParseResult, $SymbolKind>>,
+            pub rules: Vec<$crate::grammar::GrammarRule<$ParseResult, $SymbolKind>>,
         }
 
         impl $Grammar {
             $visibility fn new() -> Self {
                 let mut rules = Vec::new();
                 $(
-                    append_grammar_rules!(rules, $SymbolKind, $ParseResult, $NonTerminal, $first $($($optional_rule_tokens)* => $optional_rule_reduce)?);
+                    $crate::grammar::append_grammar_rules!(rules, $SymbolKind, $ParseResult, $NonTerminal, $({$($grouped_rule_tokens)*})? $($($optional_rule_symbol_kinds$(:$optional_rule_symbol_names)?)* => $optional_rule_reduce)?);
                 )*
                 $Grammar { rules }
             }
         }
     }
 }
+
+pub use grammar;
 
 #[cfg(test)]
 mod test {
@@ -192,6 +211,44 @@ mod test {
         assert_eq!(
             (grammar.rules[4].reduce)(vec![TestParseResult1::A, TestParseResult1::C]),
             TestParseResult1::F
+        );
+    }
+
+    grammar! {
+        grammar TestGrammar2 {
+            symbol_kind: TestSymbolKind2,
+            tokens: [A],
+            parse_result: TestParseResult2,
+            rules: {
+                ParsedA: A:a => TestParseResult2::Parsed(a.kind()),
+            }
+        }
+    }
+
+    #[derive(Debug, PartialEq)]
+    enum TestParseResult2 {
+        Token(TestSymbolKind2),
+        Parsed(TestSymbolKind2),
+    }
+
+    impl ParseResult<TestSymbolKind2> for TestParseResult2 {
+        fn kind(&self) -> TestSymbolKind2 {
+            match self {
+                TestParseResult2::Token(TestSymbolKind2::A) => TestSymbolKind2::A,
+                TestParseResult2::Parsed(TestSymbolKind2::A) => TestSymbolKind2::ParsedA,
+                _ => panic!("Unexpected parse result: {:?}", self),
+            }
+        }
+    }
+
+    #[test]
+    fn grammar_macro_captured_input() {
+        let grammar = TestGrammar2::new();
+        assert_eq!(grammar.rules.len(), 1);
+        assert_eq!(grammar.rules[0].input, vec![TestSymbolKind2::A]);
+        assert_eq!(
+            (grammar.rules[0].reduce)(vec![TestParseResult2::Token(TestSymbolKind2::A)]),
+            TestParseResult2::Parsed(TestSymbolKind2::A)
         );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
 mod grammar;
 
+use grammar::ParseResult;
+
+enum MyParseResult {
+    Token(MySymbolKind),
+    Parsed(MySymbolKind),
+}
+
+impl grammar::ParseResult<MySymbolKind> for MyParseResult {
+    fn kind(&self) -> MySymbolKind {
+        match self {
+            MyParseResult::Token(kind) => *kind,
+            MyParseResult::Parsed(kind) => *kind,
+        }
+    }
+}
+
+grammar::grammar! {
+    grammar MyGrammar {
+        symbol_kind: MySymbolKind,
+        tokens: [A, B],
+        parse_result: MyParseResult,
+        rules: {
+            ParsedA: A:a => MyParseResult::Parsed(a.kind()),
+            ParsedB: B:b => MyParseResult::Parsed(b.kind()),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let grammar = MyGrammar::new();
+    println!("{:?}", grammar);
 }
